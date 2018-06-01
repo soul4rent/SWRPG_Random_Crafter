90593746bee7b412ba9b67ef428a91ea4d0a4d0b
soul4rent <kyle.a.wadsworth@gmail.com>
2018-06-01 13:42:12 -0700

    astrogation simulator proof of concept works, need to add more features

--- AstrogationSimulator.py
+++ AstrogationSimulator.py
@@ -8,6 +8,22 @@ black = int(input("black: "))
 purple = int(input("purple: "))
 red = int(input("red: "))
 
+
+
+#0 dice is a "simple" check, 5 dice is a "formidable" check.
+checkDifficulty = max(0, min(purple+red, 5))
+
+#assuming each round is 1 minute
+astroTime = [1.0, 2.0, 5.0, 10.0, 60.0, 2400.0]
+
+
+AT = astroTime[checkDifficulty]
+
 result = dice.roll(blue, green, yellow, black, purple, red)
 
-print result
+
+
+print (result)
+
+print ("Base Time:", AT)
+print ("New Time:", AT - .10*result['a']*AT) 
